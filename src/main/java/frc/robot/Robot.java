7b53f6c58d0bd2b69713dbdf4f2f96aba186d578
Team6665 <steve.nixon@santacatalina.org>
2022-02-07 16:58:10 -0800

    quick update

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,27 +1,12 @@
 //Carbotta Protoype POC Code
 //This should make Carbotta drive with an Xbox controller, but nothing else.
 //Prior to running, verify that the motor controllers are configured and set to run using the constants below
-
-
-//IMPORTANT
-//This will run for two seconds during the autonomous mode. Then it will sit, waiting, for 13 seconds. Then, and only then,
-//can you drive this as an operator.
-
-
 package frc.robot;
 
 //Import statements for the required packages. These are the bare minumum required to make an FRC robot run
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-//import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -32,29 +17,8 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * DO NOT MESS WITH THIS NAME!!!
  */
 public class Robot extends TimedRobot {
-
-  //Creating the date structure that refer to the 4 motors (and controllers) that run the drivetrain. 
-  private final MotorController frontLeftMotor = (MotorController) new VictorSPX(0);
-  private final MotorController backLeftMotor = (MotorController) new VictorSPX(1);
-  private final MotorController frontRightMotor = (MotorController) new VictorSPX(2);
-  private final MotorController backRightMotor = (MotorController) new VictorSPX(3);
-
-  //Group the motors on each side together as controller groups to allow for combined control
-  private final MotorControllerGroup leftMotors = new  MotorControllerGroup(frontLeftMotor,backLeftMotor);
-  private final MotorControllerGroup rightMotors = new MotorControllerGroup(frontRightMotor, backRightMotor);
-
-  //Establish the control scheme as being differential. Allows each side to operate independantly of each other.
-  private final DifferentialDrive robotDrive = new DifferentialDrive(leftMotors, rightMotors);
-  
-  //Create the data structure that refers to the driver controller.
-  private final XboxController driverController = new XboxController(0);
-  //private final Joystick driverStick = new Joystick(0);
-
-  //private final Timer timer = new Timer();
-  
-  //Not useful in this program. Don't worry about these for now
-  //private Command m_autonomousCommand;
-  //private RobotContainer m_robotContainer;
+  private RobotContainer robotContainer;
+  private Command autonomousCommand;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -62,14 +26,10 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-
-    //Set the two right motors to turn backwards when given a positive voltage. Consult with Euclid as to why you need to run one set of motors in reverse.
-    frontRightMotor.setInverted(true);
-    backRightMotor.setInverted(true);
-
-    //m_robotContainer = new RobotContainer();
+    robotContainer = new RobotContainer();
   }
 
   /**
@@ -98,25 +58,18 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    // timer.reset();
-    // timer.start();
-    // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    autonomousCommand = robotContainer.getAutonomousCommand();
 
-    // // schedule the autonomous command (example)
-    // if (m_autonomousCommand != null) {
-    //   m_autonomousCommand.schedule();
-    // }
+    // schedule the autonomous command (example)
+    if (autonomousCommand != null) {
+      autonomousCommand.schedule();
+    }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    // if (timer.get() < 2.0) {
-    //   robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-    // } else {
-    //   robotDrive.stopMotor(); // stop robot
-    //}
+
   }
 
   @Override
@@ -125,18 +78,14 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    //if (m_autonomousCommand != null) {
-    //  m_autonomousCommand.cancel();
-    //}
+    if (autonomousCommand != null) {
+     autonomousCommand.cancel();
+    }
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-
-    //Established the driver control scheme. Should be very similar to most driving games on an Xbox or PS.
-    robotDrive.arcadeDrive(-driverController.getLeftY(), driverController.getRightX());
-    //robotDrive.arcadeDrive(-deriverStick.getY(), driverStick.getX());
 
   }
 
