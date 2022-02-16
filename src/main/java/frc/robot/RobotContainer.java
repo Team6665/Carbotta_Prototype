02c21699f9694b72271e79aaaf36e31b2291ccbc
Coding Lead <nunsnbolts@santacatalina.org>
2022-02-15 17:41:06 -0800

    won't drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -63,7 +63,10 @@ public class RobotContainer {
     //new JoystickButton(driverController, Button.kX.value).whenHeld(new IntakeRun(intake, 0.75));
     new JoystickButton(driverController, Button.kA.value).whenHeld(new FireLauncher(launcher, 0.75));
     
-    new JoystickButton(driverController, Button.kB.value).whenHeld(new HalveDriveSpeed(DriveTrain)); 
+    new JoystickButton(driverController, Button.kB.value).whenHeld(new HalveDriveSpeed(DriveTrain));
+    //final JoystickButton dpadUp = new JoystickButton(driverController, 5);
+    
+    //dpadUp.whenHeld(new FireLauncher(launcher, 0.50));
   }
 
   /**
