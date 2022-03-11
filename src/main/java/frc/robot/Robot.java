fc2b7798794c16767e685eb2b1f9e1add84dcf70
Rain Hu <you@example.com>
2022-03-10 17:14:16 -0800

    change in encoder

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,21 +4,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj.CounterBase;//encoder
-import edu.wpi.first.wpilibj.Encoder;//encoder
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;//encoder data connect to the smartdashboard
 
 public class Robot extends TimedRobot {
   private RobotContainer robotContainer;
   private Command autonomousCommand;
-  private final Encoder m_encoder = new Encoder(1,2,false,CounterBase.EncodingType.k4X);//Encoder(port1, port2, direction(true inversed, false default),CounterBase.Encoding Type, encoding rate (k1X,k2X,k4X(the highest it can goes)))
 
   @Override
   public void robotInit() {
     robotContainer = new RobotContainer();
-    m_encoder.setSamplesToAverage(10);//(1-255) larger number means smoother, but might be less accurate
-    m_encoder.setDistancePerPulse(1/360*6*Math.PI);//how far the mechanism attached to the encoder moves per pulse(unit in inches): 1/360countes*diameter*Math.PI
-    m_encoder.setMinRate(1);//distance in inches/second; this is the lowest rate the encoder will not stop 
+  
   }
 
   @Override
@@ -55,8 +49,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    SmartDashboard.putNumber("encoderDistance",m_encoder.getDistance());
-    SmartDashboard.putNumber("encoderRate",m_encoder.getRate());
+   
 
   }
 
