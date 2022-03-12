de0c93860640826c66fa3497efc86fd3521df8a1
Rain Hu <you@example.com>
2022-03-12 14:40:49 -0800

    Limelight

---
+++ src/main/java/frc/robot/subsytems/LimelightSubsystem.java
@@ -0,0 +1,27 @@
+package frc.robot.subsytems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+//import frc.robot.lib.GoalNotFoundException;
+
+
+public class LimelightSubsystem extends SubsystemBase {
+    final NetworkTable limelight;
+    public LimelightSubsystem (String networkTableName) {
+        this.limelight = NetworkTableInstance.getDefault().getTable(networkTableName);
+    }
+    // public double getLimelightOutput(int index) throws GoalNotFoundException{
+
+    // }
+
+    
+    // public double getDistance(){
+    //     try{
+    //         return getLimelightOutput
+    //     }
+    }
+
+
+    
