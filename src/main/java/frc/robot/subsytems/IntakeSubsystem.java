94568543bae1ad07c8f9e5672c50751fabdf5b69
Rain Hu <you@example.com>
2022-03-12 14:40:56 -0800

    Intake

--- src/main/java/frc/robot/subsytems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsytems/IntakeSubsystem.java
@@ -6,9 +6,12 @@ package frc.robot.subsytems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import frc.robot.Constants;
 import frc.robot.Constants.DriveTrainConstants;
 import frc.robot.Constants.PneumaticsConstants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -20,24 +23,46 @@ public class IntakeSubsystem extends SubsystemBase {
 
 /** Creates a new Intake. */
   public IntakeSubsystem() {
+   // motorIntake.configFactoryDefault();
     motorIntake = new WPI_VictorSPX(DriveTrainConstants.intakeMotor);
     deployIntake = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, PneumaticsConstants.intakeFront, PneumaticsConstants.intakeBack);
-  }
+  } 
+ 
+
+
+  public boolean isRunning = false;
+  public boolean isDeployed = false;
+  public boolean isRetracted = false;
+  public boolean isInverted = false;
+    
 
   public void deploy() {
     deployIntake.set(DoubleSolenoid.Value.kForward);
+    isRetracted = false;
   }
 
   public void retract() {
     deployIntake.set(DoubleSolenoid.Value.kReverse);
+    isDeployed = false;
   }
   
+  public void invertedIntakeMotor (){
+    isInverted = !this.isInverted;
+    int sign = isInverted ? 1:-1;
+    if(isRunning) motorIntake.set(ControlMode.PercentOutput, sign * Constants.IntakeConstants.intakeMotorSpeed);
+  }
   public void set(double speed) {
     motorIntake.set(ControlMode.PercentOutput, speed);
   }
 
+  public boolean isRunning(){
+    return isRunning;
+  }
+
   @Override
   public void periodic() {
+    SmartDashboard.putBoolean("IntakeStatus", isRunning);
+
     // This method will be called once per scheduler run
   }
 }
