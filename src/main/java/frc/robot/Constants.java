8b851660bf590ab58bbfe40cd6b1999ad4a58b75
Rain Hu <you@example.com>
2022-03-18 16:15:51 -0700

    launcher port

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,7 @@ public final class Constants {
         public static final int RearRightMotorPort = 2;
         public static final int upperLauncherMotor = 13;
         //public static final int lowerLauncherMotor = 12;
-        //public static final int intakeMotor = 13;
+        public static final int intakeMotor = 13;
         public static final int kP = 1;
         public static final int kI = 1;
         public static final int kD = 1;
